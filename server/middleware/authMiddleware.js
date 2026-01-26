563b5b3844606989102307ab4557c98382eda937
Arin  Dixit <id2013663@gmail.com>
2026-01-26 23:29:59 +0530

    Add files via upload

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -14,7 +14,12 @@ const protect = asyncHandler(async (req, res, next) => {
 
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await User.findById(decoded.id).select('-password').populate('teams', '_id');
+      req.user = await User.findById(decoded.id).select('-password').populate('teams');
+
+      if (!req.user) {
+        res.status(401);
+        throw new Error('Not authorized, user not found');
+      }
 
       next();
     } catch (error) {
--- server/middleware/errorMiddleware.js
+++ server/middleware/errorMiddleware.js
@@ -6,6 +6,7 @@ const notFound = (req, res, next) => {
 
 const errorHandler = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  console.error(err); // Log the error to the console
   res.status(statusCode);
   res.json({
     message: err.message,
